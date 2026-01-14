4bc43801c2464cdfb8be807289857246d612ad65
earagon1 <earagon1>
2026-01-13 23:01:50 -0300

    interfaz de login, navegacion y conexion con clerk

--- app/src/main/java/com/example/traductorlsa/MainActivity.kt
+++ app/src/main/java/com/example/traductorlsa/MainActivity.kt
@@ -1,28 +1,24 @@
-
 package com.example.traductorlsa
 
-import android.content.Context
+import android.media.AudioManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import com.example.traductorlsa.ui.CameraScreen
-import android.media.AudioManager
+import com.example.traductorlsa.ui.LsaTranslatorApp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        // Sube el volumen de música al máximo (para TextToSpeech)
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
         val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0)
 
         setContent {
-            MaterialTheme {
-                Surface { CameraScreen() }
-            }
+            // Entry point de toda la UI Compose
+            LsaTranslatorApp()
         }
     }
-
 }
